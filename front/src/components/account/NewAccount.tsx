63259e804fb814b5457932fb0086a33318ead2a4
Jun Naruse <jun.naruse@gmail.com>
2022-02-22 15:04:50 -0500

    [ADD] Initial skeleton (Splash, About, Login, New account and Navbar)

---
+++ front/src/components/account/NewAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewAccount = () => {
+    return (
+        <h1>This is new account</h1>
+    )
+}
+
+export default NewAccount;
---
+++ front/src/components/splash/Splash.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+ 
+const Splash = () => {
+    return (
+        <h1>This is Splash</h1>
+    )
+}
+
+export default Splash;
