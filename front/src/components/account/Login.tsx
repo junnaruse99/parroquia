6ab0936333381cd93e82f9de2bbba74a0d494e5d
Jun Naruse <jun.naruse@gmail.com>
2022-02-22 16:17:29 -0500

    [ADD]o form and rounting between pages

--- front/src/components/account/Login.tsx
+++ front/src/components/account/Login.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
- 
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
     return (
-        <h1>This is Login</h1>
+        <>
+        <form>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Keep Log in</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Log In</button>
+        </form>
+        <hr />
+        <p>New to this page? <Link to='/new-account'>Sign Up</Link></p>
+        </>
     )
 }
 
--- front/src/components/account/NewAccount.tsx
+++ front/src/components/account/NewAccount.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'react-phone-number-input/style.css';
+import PhoneInput from 'react-phone-number-input';
+import { Link } from 'react-router-dom';
 
 const NewAccount = () => {
+
+    const [phone, setPhone] = useState<any>()
+
     return (
-        <h1>This is new account</h1>
+        <>
+        <form>
+        <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" className="form-control" id="firstName" placeholder="First Name" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" className="form-control" id="lastName" placeholder="First Name" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="Address">Address</label>
+            <input type="text" className="form-control" id="Address" placeholder="Address" />
+        </div>
+        <PhoneInput
+            international
+            placeholder="Enter phone number"
+            defaultCountry="PE"
+            value={phone}
+            onChange={setPhone}/>
+        <button type="submit" className="btn btn-primary">Sign In</button>
+        </form>
+        <hr />
+        <p>You already have an account? <Link to='/login'>Log In</Link></p>
+        </>
     )
 }
 
