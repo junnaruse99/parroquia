a01136b1956b454a71aaa8426ffa1e376a89733a
Jun Naruse <jun.naruse@gmail.com>
2022-02-24 12:50:05 -0500

    [ADD] New inputs for new account form

--- front/src/components/account/NewAccount.tsx
+++ front/src/components/account/NewAccount.tsx
@@ -19,20 +19,26 @@ const NewAccount = () => {
             <input type="text" className="form-control" id="lastName" placeholder="First Name" />
         </div>
         <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password" />
         </div>
         <div className="form-group">
-            <label htmlFor="Address">Address</label>
-            <input type="text" className="form-control" id="Address" placeholder="Address" />
+            <label htmlFor="confirmPassword">Confirm password</label>
+            <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm password" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input type="text" className="form-control" id="address" placeholder="Address" />
+        </div>
+        <div className="form-group">
+            <label htmlFor='phoneNumber'>Phone Number</label>
         </div>
         <PhoneInput
-            international
             placeholder="Enter phone number"
             defaultCountry="PE"
             value={phone}
